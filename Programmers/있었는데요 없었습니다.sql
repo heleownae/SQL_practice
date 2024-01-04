73c5a935f683dceaceaf86944eda6527d65b1d90
Haewon Lee <heleownae@gmail.com>
2024-01-04 14:51:42 +0900

    있었는데요 없었습니다.sql

---
+++ Programmers/있었는데요 없었습니다.sql
@@ -0,0 +1,13 @@
+-- 있었는데요 없었습니다
+select
+    a.animal_id, a.name
+from
+    animal_ins a
+join
+    animal_outs b
+on
+    a.animal_id=b.animal_id
+where
+    a.datetime > b.datetime
+order by
+a.datetime
